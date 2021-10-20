433380896a1923bd70de768f24656a753fdf2475
GitHub Action <action@github.com>
2021-10-20 13:10:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/wandk/plugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wandk/plugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wandk.plugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wandk.plugintemplate.MyBundle
 
 class MyApplicationService {
 
